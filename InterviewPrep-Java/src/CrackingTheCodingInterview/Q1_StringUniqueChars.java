1900b700215370cfb1a1aaec294ba6509479c496
Adityan <msadityan@gmail.com>
2016-12-13 00:46:00 -0500

    String has unique characters question

---
+++ InterviewPrep-Java/src/CrackingTheCodingInterview/Q1_StringUniqueChars.java
@@ -0,0 +1,31 @@
+package CrackingTheCodingInterview;
+
+
+
+import java.util.HashSet;
+
+public class Q1_StringUniqueChars {
+	
+	/*Determine if a string has all unique characters.
+	 * What if you can't use other data structures?
+	 */
+	public boolean hasUniqueChars(String input){
+		
+		HashSet<Character> set = new HashSet<>();
+		
+		for(int i=0;i<input.length();i++){
+			if(set.contains(input.charAt(i))) return false;
+			else set.add(input.charAt(i));
+		}
+		return true;
+	}
+	
+	public static void main(String a[]){
+		
+		Q1_StringUniqueChars s = new Q1_StringUniqueChars();
+		System.out.println(s.hasUniqueChars("abc"));
+		System.out.println(s.hasUniqueChars("aaba"));
+	}
+
+}
+
